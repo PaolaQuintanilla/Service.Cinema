386f32798aa9e076652d13e9f876d35967e1b42d
Paola.Quintanilla <74014.linares@gmail.com>
2021-05-20 18:45:55 -0300

    Adding Theater functions

--- cinema/Controllers/CinemaController.cs
+++ cinema/Controllers/CinemaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cinema.Criteria;
+using cinema.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -17,5 +19,37 @@ namespace cinema.Controllers
         {
             _logger = logger;
         }
+        #region Theater
+
+        [HttpGet("GetTheaters")]
+        public IEnumerable<Theater> GetTheaters()
+        {
+            var result = new List<Theater>();
+            using (cinemadbContext db = new cinemadbContext())
+            {
+                result = db.Theater.Where(f => f.IsActive == 1).ToList();
+            }
+
+            return result;
+        }
+
+        [HttpPost("CreateTheater")]
+        public async Task<ActionResult<Theater>> CreateTheater(TheaterCriteria item)
+        {
+            Theater result = new Theater();
+            using (cinemadbContext db = new cinemadbContext())
+            {
+                result.Name = item.Name;
+                result.Description = item.Description;
+                result.CreatedBy = 1;
+                result.IsActive = 1;
+                result.CreatedAt = DateTime.Now;
+                db.Add(result);
+                db.SaveChanges();
+            }
+            return result;
+        }
+        #endregion
+
     }
 }
